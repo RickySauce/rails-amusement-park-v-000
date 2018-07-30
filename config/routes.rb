f730edd89d019eb98490e81f478e9bbd9a9caaa1
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-30 12:45:49 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :admins
+  end
+
   root 'application#welcome'
   resources :attractions
   post 'ride', to: 'attractions#ride'
