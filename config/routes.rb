e3024ed1647aa06383c87063f6ac79dafbdb9ef6
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-30 11:50:31 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'application#welcome'
+  resources :attractions
   resources :users
   match 'signin' => 'users#signin', via: [:get, :post]
   get 'logout', to: 'users#logout'
