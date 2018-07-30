130d0301f7691f907f3c511429a19d1ea83b351a
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-30 11:34:39 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,15 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
   has_secure_password
-<<<<<<< HEAD
   before_create :default
-=======
+
   after_initialize :default
->>>>>>> c940a59634119d74b5adac5a20e78cc588823b58
 
   def default
     self.admin = false
   end
 
   def mood
-<<<<<<< HEAD
     if self.nausea.nil? || self.happiness.nil?
       "WHO CARES"
     else
@@ -22,12 +19,10 @@ class User < ActiveRecord::Base
       else
         "happy"
       end
-=======
     if self.nausea > self.happiness
       "sad"
     else
       "happy"
->>>>>>> c940a59634119d74b5adac5a20e78cc588823b58
     end
   end
 
